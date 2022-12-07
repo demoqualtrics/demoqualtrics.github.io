572e5b117d934d7b0ed9380354f28dc1a59b5ea6
alyc100 <alyc100@hotmail.com>
2022-12-07 18:44:45 +0000

    protector

--- finance-retail-2/js/copyprotect.js
+++ finance-retail-2/js/copyprotect.js
@@ -5,6 +5,9 @@ var protector = document.getElementById("myProtector");
 var span = document.getElementsByClassName("close")[0];
 
 // Unblock website if correct access code has been provided
-if ( window.localStorage.getItem("accessCode") === "1e7ba2d045c77e6485d6f0499f9a608da5050d707282aa21a776bc8232672c3a85bfa7632ae5677cd6e29003fed58c5c5819a12292ce759a513ae55b50542072") {
+var x = getCookie("accessCode");
+if (x) {
+    if (x === "1e7ba2d045c77e6485d6f0499f9a608da5050d707282aa21a776bc8232672c3a85bfa7632ae5677cd6e29003fed58c5c5819a12292ce759a513ae55b50542072") {
     protector.style.display = "none";
  }
+}
