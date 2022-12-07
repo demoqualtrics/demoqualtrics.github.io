c90cd781c61c478bcd674c86a172034165b9efcc
alyc100 <alyc100@hotmail.com>
2022-12-07 18:26:39 +0000

    copyprotect

---
+++ finance-retail-2/js/copyprotect.js
@@ -0,0 +1,10 @@
+// Get the modal
+var protector = document.getElementById("myProtector");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// Unblock website if correct access code has been provided
+if ( window.localStorage.getItem("accessCode") === "1e7ba2d045c77e6485d6f0499f9a608da5050d707282aa21a776bc8232672c3a85bfa7632ae5677cd6e29003fed58c5c5819a12292ce759a513ae55b50542072") {
+    protector.style.display = "none";
+ }
