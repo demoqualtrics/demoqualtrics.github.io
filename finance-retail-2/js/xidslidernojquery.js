34e648375b997681b213598a61b0f7cd845e38c6
alyc100 <alyc100@hotmail.com>
2023-02-02 15:30:55 +0000

    Create xidslidernojquery.js

---
+++ finance-retail-2/js/xidslidernojquery.js
@@ -0,0 +1,5 @@
+/*============= xidslidernojquery js css =============*/
+    // Slideout show
+document.querySelector('#slideOut').querySelector('.slideOutTab').addEventListener('click', function() {
+        document.querySelector("#slideOut").classList.toggle('showSlideOut');
+    });
